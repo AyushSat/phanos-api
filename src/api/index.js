7ed9dfdc9974e6f1b2a4af1b1af33df0f007640d
Ayush Satyavarpu <ayushsatyavarpu@gmail.com>
2025-05-02 17:55:10 -0400

    added auth on backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,38 @@
+const { CognitoJwtVerifier } = require('aws-jwt-verify');
+
 const express = require('express');
+
+const userPoolId = 'us-east-2_AqjEcBPFO';
+
+async function verifyJWT(token) {
+  try {
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId,
+      tokenUse: 'id',
+      clientId: null
+    });
+
+    const payload = await verifier.verify(token);
+    return payload;
+  } catch (err) {
+    console.error('Error verifying JWT:', err);
+  }
+}
 
 const emojis = require('./emojis');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  const payload = await verifyJWT(token);
+
   res.json({
-    message: 'Welcome to Phanos',
+    message: "From the backend, welcome " + payload["email"],
   });
 });
 
